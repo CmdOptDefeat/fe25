1ed771ee858ebe43c2c38a5ed13fde098e69f7e0
Pranav Kiran Rajarathna <pranavkr.2308@gmail.com>
2025-08-30 18:10:30 +0530

    Check if WiFi can be disabled then enabled

--- initial-tests/startup-test/led_launcher.sh
+++ initial-tests/startup-test/led_launcher.sh
@@ -1,3 +1,14 @@
+WIFI_INTERFACE="wlan0"
+
+# Turn Wi-Fi off
+/usr/sbin/ifconfig "$WIFI_INTERFACE" down
+
 cd /
+# Run script
 sudo python3 home/Pranav/Desktop/fe25/initial-tests/startup-test/blink_led.py
 cd /
+
+sleep 15
+
+# Turn Wi-Fi back on
+/usr/sbin/ifconfig "$WIFI_INTERFACE" up
